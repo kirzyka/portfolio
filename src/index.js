cb5d3362b84f53cf076c4c64e59f2c0e391e33aa
Anton Kirzyk <akirzyk@luxoft.com>
2018-02-28 17:28:37 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import registerServiceWorker from './registerServiceWorker';
-import App from './App';
 import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-function render(Component) {
-	ReactDOM.render(
-		<AppContainer>
-			<Component/>
-		</AppContainer>,
-	document.getElementById('root'));
-}
-
-render(App);
-
-if(module.hot) {
-	module.hot.accept('./App', () => { render(App);});
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
